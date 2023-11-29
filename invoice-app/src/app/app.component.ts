afe7d74629313dffde9d4ffe98db025b21ea4a39
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2023-11-29 22:48:16 +0100

    Zajecia 6 Zadanie 1

--- invoice-app/src/app/app.component.ts
+++ invoice-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CustomerFormComponent } from './components/customer-form/customer-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CustomerFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ invoice-app/src/app/components/customer-form/customer-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-customer-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './customer-form.component.html',
+  styleUrl: './customer-form.component.scss'
+})
+export class CustomerFormComponent {
+
+}
