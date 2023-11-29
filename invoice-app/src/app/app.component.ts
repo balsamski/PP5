42e859c61c16d3ea557c5bf476517b1ca3791ba5
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2023-11-29 22:37:12 +0100

    Zajecia 6 Zadanie 1

--- invoice-app/src/app/app.component.ts
+++ invoice-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ClientFormComponent } from './client-form/client-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ClientFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
