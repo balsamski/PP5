34e5c0866bb6af5052dd19816925463d216095f0
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2024-01-17 23:12:05 +0100

    Zajęcia 12 Zadanie 2

--- invoice-app/src/app/invoice/services/customer.service.ts
+++ invoice-app/src/app/invoice/services/customer.service.ts
@@ -5,43 +5,37 @@ import { Observable, map } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerService {
-
   url: string = `${environment.apiUrl}${environment.indexNo}/customers/`;
   customersList: Customer[] = [];
 
-  constructor(
-    private httpClient: HttpClient
-  ) { 
-    console.log(this.url)
+  constructor(private httpClient: HttpClient) {
+    console.log(this.url);
   }
 
   addCustomer(customer: Customer): Observable<Customer[]> {
-    return this.httpClient.post<Customer[]>(
-      this.url,
-      customer
-    );
+    return this.httpClient.post<Customer[]>(this.url, customer);
   }
 
-  getCustomers():  Observable<Customer[]>	 {
-    return this.httpClient.get<Customer[]>(
-      this.url
-    ).pipe(
-      map(
-        (customers: Customer[]) => customers.map(customer => new Customer().deserialize(customer))
-      )
-    )
+  getCustomers(): Observable<Customer[]> {
+    return this.httpClient
+      .get<Customer[]>(this.url)
+      .pipe(
+        map((customers: Customer[]) =>
+          customers.map((customer) => new Customer().deserialize(customer))
+        )
+      );
   }
-  
+
   removeCustomer(removedCustomer: Customer) {
     const headers = new HttpHeaders({
-      'Authorization': 'haslo_haslo'
+      Authorization: 'haslo_haslo',
     });
 
-    return this.httpClient.delete<Customer[]>(
-      this.url + removedCustomer.nip, { headers }
-    );
+    return this.httpClient.delete<Customer[]>(this.url + removedCustomer.nip, {
+      headers,
+    });
   }
 }
